3f1fdabf38aa897ab1dbfa3291ea980f5b4e1966
yashsharma127 <yashshar.2323@gmail.com>
2023-07-20 00:07:42 +0530

    added test data array and a navbar div

--- hiring-front/src/App.js
+++ hiring-front/src/App.js
@@ -1,6 +1,23 @@
 import coverimg from "./images/cover.jpg"
 
 const App = () => {
+  const emp = [
+    {
+      id:'1',
+      name: 'yash',
+      content: 'HTML is easy'
+    },
+    {
+      id:'2',
+      name: 'harsh',
+      content: 'Browser can execute only JavaScript'
+    },
+    {
+      id:'3',
+      name: 'arsh',
+      content: 'GET and POST are the most important methods of HTTP protocol'
+    }
+  ]
   return (
     <div className=" gap-8 bg-gray-300 min-h-screen p-16">
     <nobr><h1 className="text-5xl text-center  font-bold ">COMPUTER SCIENCE AND ENGINEERING - HIRING PORTAL</h1></nobr> 
---
+++ hiring-front/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css';
+
+const Navbar = () => {
+    return(
+
+        <nav className='main-nav flex bg-white shadow-md h-24 items-center py-4 px-8 '  >
+            
+                
+                
+        </nav>)
+}
+
+export default Navbar
