b262cbe3485b6cfe90a61ecf229ec67744bf6e00
yashsharma127 <yashshar.2323@gmail.com>
2023-07-20 00:17:26 +0530

    adding footer to the page

--- hiring-front/src/components/Navbar.js
+++ hiring-front/src/components/Navbar.js
@@ -10,16 +10,15 @@ const Navbar = () => {
                 <a href='../index.js'>
                 <img className='h-12 w-50' src={logo} alt="MRlogo"/>
                 </a>
-                </div>
-                <div className='ml-auto'>
+            </div>
+            <div className='ml-auto'>
                     <button
                     className='  bg-[#94221f] rounded-md font-bold uppercase text-sm px-6 py-3 text-white
                     active:bg-rose-600 hover:bg-rose-700 rounded shadow-md 
                     mr-1 mb-1 '>Log Out</button>
-                </div>
-                
-                
-        </nav>)
+            </div>    
+        </nav>
+    )
 }
 
 export default Navbar
