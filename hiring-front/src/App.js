7b0b456e6616fdce4d1b22f0a6ada54d2a0c3823
yashsharma127 <yashshar.2323@gmail.com>
2023-07-20 00:02:58 +0530

    added basic front

--- hiring-front/src/App.js
+++ hiring-front/src/App.js
@@ -1,9 +1,18 @@
+import coverimg from "./images/cover.jpg"
 
 const App = () => {
   return (
-    <div className="App">
-      here
-    </div>
+    <div className=" gap-8 bg-gray-300 min-h-screen p-16">
+    <nobr><h1 className="text-5xl text-center  font-bold ">COMPUTER SCIENCE AND ENGINEERING - HIRING PORTAL</h1></nobr> 
+      <div>
+        <img className="pt-20 ml-80" src={coverimg} alt="Image not found"/>
+      </div>
+      <div className="grid grid-rows gap-8 min-h-screen pt-28 p-16">
+       
+        
+      </div>
+
+  </div>
   );
 }
 
