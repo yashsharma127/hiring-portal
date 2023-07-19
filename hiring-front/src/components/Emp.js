dadff5f81f9341ea712784c35409c6b31a874d67
yashsharma127 <yashshar.2323@gmail.com>
2023-07-20 00:24:44 +0530

    adding the Employee data field card

--- hiring-front/src/components/Emp.js
+++ hiring-front/src/components/Emp.js
@@ -1,11 +1,26 @@
-
+import goku from "../images/goku.png"
 
 const Projects = ({empdata}) => {
     console.log(empdata)
     return (
         <>
-           
-                
+           {empdata.map(data => {
+            return(
+                <div  key={data.id}
+                    className=" ">
+                    
+                    <img className="" src={goku} alt="Employee image"/>
+                    <div className="">
+                    <h1 className="">{data.name}</h1>
+                            <p className="hero-text">{data.content}</p>
+                            <a href="#"><button className="">resume</button></a>
+                            
+                    </div>
+                </div>
+            )
+                }
+              )
+            }     
         </>
           )
 }
