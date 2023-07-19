9c09d97b56d72eeb78fb705d5d6288e081b7458a
yashsharma127 <yashshar.2323@gmail.com>
2023-07-20 00:30:57 +0530

    added styling to the emplyee card

--- hiring-front/src/components/Emp.js
+++ hiring-front/src/components/Emp.js
@@ -7,13 +7,13 @@ const Projects = ({empdata}) => {
            {empdata.map(data => {
             return(
                 <div  key={data.id}
-                    className=" ">
+                    className=" rounded-lg shadow-lg bg-white transition duration-500 h-52 hover:scale-110 p-16">
                     
-                    <img className="" src={goku} alt="Employee image"/>
-                    <div className="">
-                    <h1 className="">{data.name}</h1>
+                    <img className="absolute h-28 w-28 -mt-4 rounded-lg" src={goku} alt="Girl in a jacket"/>
+                    <div className="absolute ml-44">
+                    <h1 className="font-bold text-3xl text-black uppercase">{data.name}</h1>
                             <p className="hero-text">{data.content}</p>
-                            <a href="#"><button className="">resume</button></a>
+                            <a href="#"><button className="bg-sky-500 hover:bg-sky-400 rounded-lg w-20">resume</button></a>
                             
                     </div>
                 </div>
